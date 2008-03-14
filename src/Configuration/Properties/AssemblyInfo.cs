d6794ab8c92055f6aeb728f4a411a4abf3b0796e
Nate Kohari <nate@enkari.com>
2008-03-14 02:48:55 +0000

    Removed more cruft from the trunk. Will be developing configuration extension in a branch.
    git-svn-id: https://ninject.googlecode.com/svn/trunk@17 120300b3-9e3e-0410-8997-0939a4f6c7b0

--- src/Configuration/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security.Permissions;
-[assembly: AssemblyTitle("Ninject Configuration Library")]
-[assembly: ComVisible(false)]
-[assembly: Guid("5329d569-4258-4169-86ce-b909dfe52ead")]
-[assembly: CLSCompliant(true)]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum)]
