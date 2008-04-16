2ca73575961772ee8a67b4270fa887d3ad907e41
Nate Kohari <nate@enkari.com>
2008-04-16 00:50:18 +0000

    Added NLog integration extension by Ivan Porto Carrero
    git-svn-id: https://ninject.googlecode.com/svn/trunk@45 120300b3-9e3e-0410-8997-0939a4f6c7b0

---
+++ src/Integration/NLog/NLogModule.cs
@@ -0,0 +1,50 @@
+#region License
+//
+// Author: Ivan Porto Carrero <ivan@flanders.co.nz>
+// Copyright (c) 2008, Flanders International Marketing Ltd.
+// Copyright (c) 2007, Enkari, Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+#region Usings
+using System;
+using Ninject.Core;
+using Ninject.Core.Logging;
+using Ninject.Integration.NLog.Infrastructure;
+#endregion
+
+namespace Ninject.Integration.NLog
+{
+	/// <summary>
+	/// Extends the functionality of the kernel, providing integration between the Ninject
+	/// logging infrastructure and nlog.
+	/// </summary>
+	public class NLogModule : StandardModule
+	{
+		/// <summary>
+		/// Prepares the module for being loaded. Can be used to connect component dependencies.
+		/// </summary>
+		public override void BeforeLoad()
+		{
+			Kernel.Connect<ILoggerFactory>(new NLogLoggerFactory());
+		}
+
+		/// <summary>
+		/// Loads the module into the kernel.
+		/// </summary>
+		public override void Load()
+		{
+		}
+	}
+}
