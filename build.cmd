f91189444090a5806ac4983b3bfee0aadf69a24c
danielmarbach <daniel.marbach@openplace.net>
2010-06-06 19:41:30 +0200

    Modified build: (1) SharedBuildAssemblyInfo.cs is added to git and reverted after build or fail (2) Added git.exe to tools for reverting target (3) Echoed build.version to teamcity according to ninject2

--- build.cmd
+++ build.cmd
@@ -1,2 +1,21 @@
 @echo off
-tools\nant\nant.exe -buildfile:ninject.build %1 %2 %3 %4 %5 %6 %7 %8
+set nantfile=Ninject.build
+set nantexe=tools\nant\nant.exe
+
+%nantexe% -buildfile:%nantfile% %1 %2 %3 %4 %5 %6 %7 %8
+IF ERRORLEVEL 1 GOTO Failed
+
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+IF ERRORLEVEL 1 GOTO Failed
+
+echo "Release build completed."
+GOTO End
+
+:Failed
+%nantexe% -buildfile:%nantfile% -q -nologo revert
+echo "============================================================"
+echo "BUILD FAILED"
+echo "============================================================"
+
+:End
+pause
